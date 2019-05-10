5b08e3a166bb959371352ccb81d78b0fe08e5760
bjorn3 <bjorn3@users.noreply.github.com>
2019-05-10 17:46:56 +0200

    [WIP] nicer WriteBackendMethods

--- src/librustc_codegen_ssa/traits/write.rs
+++ src/librustc_codegen_ssa/traits/write.rs
@@ -6,12 +6,43 @@ use rustc::dep_graph::WorkProduct;
 use rustc_errors::{FatalError, Handler};
 
 pub trait WriteBackendMethods: 'static + Sized + Clone {
-    type Module: Send + Sync;
-    type TargetMachine;
-    type ModuleBuffer: ModuleBufferMethods;
     type Context: ?Sized;
+    type TargetMachine;
+
+    type Module: Send + Sync;
+    type OptimizedModule: Send + Sync;
+
+    type ModuleBuffer: ModuleBufferMethods;
     type ThinData: Send + Sync;
     type ThinBuffer: ThinBufferMethods;
+
+    /// Optimize this module in isolation
+    unsafe fn optimize(
+        cgcx: &CodegenContext<Self>,
+        diag_handler: &Handler,
+        module: ModuleCodegen<Self::Module>,
+        config: &ModuleConfig,
+    ) -> Result<ModuleCodegen<Self::OptimizedModule>, FatalError>;
+
+    /// Emit an object file
+    unsafe fn codegen(
+        cgcx: &CodegenContext<Self>,
+        diag_handler: &Handler,
+        module: ModuleCodegen<Self::OptimizedModule>,
+        config: &ModuleConfig,
+    ) -> Result<CompiledModule, FatalError>;
+
+    // ==== single module operations for LTO ====
+
+    fn prepare_thin(
+        module: ModuleCodegen<Self::Module>
+    ) -> (String, Self::ThinBuffer);
+
+    fn serialize_module(
+        module: ModuleCodegen<Self::OptimizedModule>
+    ) -> (String, Self::ModuleBuffer);
+
+    // ==== cross module operations for LTO ====
 
     /// Performs fat LTO by merging all modules into a single one and returning it
     /// for further optimization.
@@ -20,6 +51,7 @@ pub trait WriteBackendMethods: 'static + Sized + Clone {
         modules: Vec<FatLTOInput<Self>>,
         cached_modules: Vec<(SerializedModule<Self::ModuleBuffer>, WorkProduct)>,
     ) -> Result<LtoModuleCodegen<Self>, FatalError>;
+
     /// Performs thin LTO by performing necessary global analysis and returning two
     /// lists, one of the modules that need optimization and another for modules that
     /// can simply be copied over from the incr. comp. cache.
@@ -28,35 +60,22 @@ pub trait WriteBackendMethods: 'static + Sized + Clone {
         modules: Vec<(String, Self::ThinBuffer)>,
         cached_modules: Vec<(SerializedModule<Self::ModuleBuffer>, WorkProduct)>,
     ) -> Result<(Vec<LtoModuleCodegen<Self>>, Vec<WorkProduct>), FatalError>;
-    fn print_pass_timings(&self);
-    unsafe fn optimize(
+
+    // ====
+
+    fn run_lto_pass_manager(
         cgcx: &CodegenContext<Self>,
-        diag_handler: &Handler,
-        module: &ModuleCodegen<Self::Module>,
+        llmod: ModuleCodegen<Self::Module>,
         config: &ModuleConfig,
-    ) -> Result<(), FatalError>;
+        thin: bool,
+    ) -> ModuleCodegen<Self::OptimizedModule>;
+
     unsafe fn optimize_thin(
         cgcx: &CodegenContext<Self>,
         thin: &mut ThinModule<Self>,
     ) -> Result<ModuleCodegen<Self::Module>, FatalError>;
-    unsafe fn codegen(
-        cgcx: &CodegenContext<Self>,
-        diag_handler: &Handler,
-        module: ModuleCodegen<Self::Module>,
-        config: &ModuleConfig,
-    ) -> Result<CompiledModule, FatalError>;
-    fn prepare_thin(
-        module: ModuleCodegen<Self::Module>
-    ) -> (String, Self::ThinBuffer);
-    fn serialize_module(
-        module: ModuleCodegen<Self::Module>
-    ) -> (String, Self::ModuleBuffer);
-    fn run_lto_pass_manager(
-        cgcx: &CodegenContext<Self>,
-        llmod: &ModuleCodegen<Self::Module>,
-        config: &ModuleConfig,
-        thin: bool,
-    );
+
+    fn print_pass_timings(&self);
 }
 
 pub trait ThinBufferMethods: Send + Sync {
