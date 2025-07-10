e2ae39713576e375bf93a35504cf4d12fa03d400
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-07-10 13:34:32 +0000

    Bootstrap from rustc 1.87

--- compiler/rustc_resolve/src/lib.rs
+++ compiler/rustc_resolve/src/lib.rs
@@ -15,6 +15,7 @@
 #![feature(arbitrary_self_types)]
 #![feature(assert_matches)]
 #![feature(box_patterns)]
+#![cfg_attr(bootstrap, feature(cell_update))]
 #![feature(if_let_guard)]
 #![feature(iter_intersperse)]
 #![feature(rustc_attrs)]
--- src/bootstrap/src/core/builder/cargo.rs
+++ src/bootstrap/src/core/builder/cargo.rs
@@ -586,6 +586,7 @@ impl Builder<'_> {
             }
             rustflags.env("RUSTFLAGS_BOOTSTRAP");
             rustflags.arg("--cfg=bootstrap");
+            rustflags.arg("-Zcrate-attr=feature(let_chains)");
         }
 
         if cmd_kind == Kind::Clippy {
