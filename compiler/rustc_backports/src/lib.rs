0afed78c22770589f4f9ad6d6310c385073ca79e
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-08-14 11:45:35 +0000

    Introduce rustc_backports crate

---
+++ compiler/rustc_backports/src/lib.rs
@@ -0,0 +1,3 @@
+//! This crate defines various extension traits and macros to backport newer
+//! standard library features to older rustc versions to allow bootstrapping
+//! rustc from older rustc versions.
